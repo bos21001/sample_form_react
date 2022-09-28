a56d5427542473d4184b280f53e532777536afd8
Christopher Mendes <bos21001@byui.edu>
2022-09-28 00:53:32 -0300

    feat: Added handleSubmit function

--- src/index.js
+++ src/index.js
@@ -11,62 +11,82 @@ function Title(props) {
     )
 }
 
-function NicknameInput() {
+function InputForm(props) {
     return (
         <div className='mb-3'>
-            <label for='nicknameInput' className='form-label fw-semibold'>Nickname</label>
-            <input type='text' className='form-control shadow-sm' name='nickname' id='nicknameInput' placeholder='Tito' required />
+            <label for={props.for} className='form-label fw-semibold'>{props.label}</label>
+            {props.children}
         </div>
     );
 }
 
-function AgeInput() {
-    return (
-        <div className='mb-3'>
-            <label for='ageInput' className='form-label fw-semibold'>Age</label>
-            <input type='number' className='form-control shadow-sm' name='age' id='ageInput' placeholder='23' required />
-        </div>
-    );
-}
+class SampleForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nickname: '',
+            age: '',
+            email: '',
+            note: ''
+        };
+        this.title = props.title;
 
-function EmailInput() {
-    return (
-        <div className='mb-3'>
-            <label for='emailInput' className='form-label fw-semibold'>Email address</label>
-            <input type='email' className='form-control shadow-sm' name='email' id='emailInput' placeholder='name@example.com' />
-        </div>
-    );
-}
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-function NoteInput() {
-    return (
-        <div className='mb-3'>
-            <label for='noteTextarea' className='form-label fw-semibold'>Note</label>
-            <textarea className='form-control shadow-sm' name='note' id='noteTextarea' rows='2'></textarea>
-        </div>
-    );
-}
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
 
-function App(props) {
-    return (
-        <div className='container p-0 card bg-light position-absolute top-50 start-50 translate-middle shadow-lg'>
-            <div className='card-body'>
-                <form action='post'>
-                    <Title title={props.title} />
-                    <NicknameInput />
-                    <AgeInput />
-                    <EmailInput />
-                    <NoteInput />
-                    <div className='text-center'>
-                        <input type='submit' value='Submit' className='btn btn-dark shadow-sm' />
-                    </div>
-                </form>
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert(this.state.nickname + " " + this.state.age + " " + this.state.email + " " + this.state.note);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className='container p-0 card bg-light position-absolute top-50 start-50 translate-middle shadow-lg'>
+                <div className='card-body'>
+                    <form onSubmit={this.handleSubmit}>
+
+                        <Title title={this.title} />
+
+                        <InputForm for='nickname' label='Nickname'>
+                            <input type='text' value={this.state.nickname} onChange={this.handleInputChange} className='form-control shadow-sm' name='nickname' id='nickname' placeholder='Tito' required />
+                        </InputForm>
+
+                        <InputForm for='age' label='Age'>
+                            <input type='number' value={this.state.age} onChange={this.handleInputChange} className='form-control shadow-sm' name='age' id='age' placeholder='23' required />
+                        </InputForm>
+
+                        <InputForm for='email' label='Email Adress'>
+                            <input type='email' value={this.state.email} onChange={this.handleInputChange} className='form-control shadow-sm' name='email' id='email' placeholder='name@example.com' />
+                        </InputForm>
+
+                        <InputForm for='note' label='Note'>
+                            <textarea value={this.state.note} onChange={this.handleInputChange} className='form-control shadow-sm' name='note' id='note' rows='2'></textarea>
+                        </InputForm>
+
+                        <div className='text-center'>
+                            <input type='submit' value='Submit' className='btn btn-dark shadow-sm' />
+                        </div>
+
+                    </form>
+                </div>
+                <div class="card-footer text-center">
+                    © 2022 - Christopher Mendes - Made with Bootstrap and React
+                </div>
             </div>
-            <div class="card-footer text-center">
-                © 2022 - Christopher Mendes - Made with Bootstrap and React
-            </div>
-        </div>
-    );
+
+        );
+    }
 }
 
 const title = {
@@ -76,7 +96,7 @@ const title = {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App
+    <SampleForm
         title={title}
     />
 );
