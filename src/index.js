ab5aba79c7c992d4ebd473419c067f23a1b5a861
Christopher Mendes <bos21001@byui.edu>
2022-09-29 00:51:58 -0300

    refactor: Refactored to better apply reactjs patterns

--- src/index.js
+++ src/index.js
@@ -1,102 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
+import App from './App';
 
-function Title(props) {
-    return (
-        <div className='mb-3'>
-            <h4 class='card-title'>{props.title.title}</h4>
-            <h6 class='card-subtitle mb-2 text-muted'>{props.title.subtitle}</h6>
-        </div>
-    )
-}
-
-function InputForm(props) {
-    return (
-        <div className='mb-3'>
-            <label for={props.for} className='form-label fw-semibold'>{props.label}</label>
-            {props.children}
-        </div>
-    );
-}
-
-class SampleForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nickname: '',
-            age: '',
-            email: '',
-            note: ''
-        };
-        this.title = props.title;
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    handleSubmit(event) {
-        alert(this.state.nickname + " " + this.state.age + " " + this.state.email + " " + this.state.note);
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div className='container p-0 card bg-light position-absolute top-50 start-50 translate-middle shadow-lg'>
-                <div className='card-body'>
-                    <form onSubmit={this.handleSubmit}>
-
-                        <Title title={this.title} />
-
-                        <InputForm for='nickname' label='Nickname'>
-                            <input type='text' value={this.state.nickname} onChange={this.handleInputChange} className='form-control shadow-sm' name='nickname' id='nickname' placeholder='Tito' required />
-                        </InputForm>
-
-                        <InputForm for='age' label='Age'>
-                            <input type='number' value={this.state.age} onChange={this.handleInputChange} className='form-control shadow-sm' name='age' id='age' placeholder='23' required />
-                        </InputForm>
-
-                        <InputForm for='email' label='Email Adress'>
-                            <input type='email' value={this.state.email} onChange={this.handleInputChange} className='form-control shadow-sm' name='email' id='email' placeholder='name@example.com' />
-                        </InputForm>
-
-                        <InputForm for='note' label='Note'>
-                            <textarea value={this.state.note} onChange={this.handleInputChange} className='form-control shadow-sm' name='note' id='note' rows='2'></textarea>
-                        </InputForm>
-
-                        <div className='text-center'>
-                            <input type='submit' value='Submit' className='btn btn-dark shadow-sm' />
-                        </div>
-
-                    </form>
-                </div>
-                <div class="card-footer text-center">
-                    © 2022 - Christopher Mendes - Made with Bootstrap and React
-                </div>
-            </div>
-
-        );
-    }
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const title = {
     title: 'Sample Form React',
     subtitle: 'Let me "know" you 😉'
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <SampleForm
-        title={title}
-    />
+    <React.StrictMode>
+        <App
+            title={title}
+        />
+    </React.StrictMode>
 );
