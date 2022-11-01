820693c14dbb9e1a0e0fe41b339d428c21f088aa
Christopher Mendes <bos21001@byui.edu>
2022-11-01 20:23:37 -0300

    feat: Added a simple breadcrumb on the top of SubmittedPage

--- src/SubmittedPage.js
+++ src/SubmittedPage.js
@@ -25,26 +25,35 @@ export default function SubmittedPage(props) {
 
 
     return (
-        <div className='container p-0 card bg-light position-absolute top-50 start-50 translate-middle shadow-lg'
-             style={{"maxWidth": "40rem"}}>
-            <FetchGetAllSampleForms token={props.access_token} data={location}/>
-            <div className='text-center'>
-                <h4>Thank You!</h4>
-                <span className="bi bi-check-circle-fill text-success h1"/>
-                <h5 className="text-secondary">Your data has been sent.</h5>
-                <h6 className="text-secondary">Checkout bellow what we received</h6>
-            </div>
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item active"><a href="/" className={"text-warning"}>Home</a></li>
+                    <li className="breadcrumb-item active"><a href="/get-all-sample-form" className={"text-warning"}>Get All</a></li>
+                    <li className="breadcrumb-item" aria-current="page">Submitted</li>
+                </ol>
+            </nav>
+            <div className='container p-0 card bg-light position-absolute top-50 start-50 translate-middle shadow-lg'
+                 style={{"maxWidth": "40rem"}}>
+                <FetchGetAllSampleForms token={props.access_token} data={location}/>
+                <div className='text-center'>
+                    <h4>Thank You!</h4>
+                    <span className="bi bi-check-circle-fill text-success h1"/>
+                    <h5 className="text-secondary">Your data has been sent.</h5>
+                    <h6 className="text-secondary">Checkout bellow what we received</h6>
+                </div>
 
-            <div className="card-footer text-muted m-0">
-                <h6 className='text-center fw-semibold'>Received Data:</h6>
-                <hr className="mt-0"></hr>
-                <p className='fw-light'><strong className='fw-semibold'>Nickname:</strong> {nickname}</p>
-                <p className='fw-light'><strong className='fw-semibold'>Age:</strong> {age}</p>
-                <p className='fw-light'><strong className='fw-semibold'>Email:</strong> {email}</p>
-                <p className='fw-light'><strong className='fw-semibold'>Note:</strong> {note}</p>
-            </div>
-            <div className="card-footer text-center">
-                © 2022 - Christopher Mendes - Made with Bootstrap and React
+                <div className="card-footer text-muted m-0">
+                    <h6 className='text-center fw-semibold'>Received Data:</h6>
+                    <hr className="mt-0"></hr>
+                    <p className='fw-light'><strong className='fw-semibold'>Nickname:</strong> {nickname}</p>
+                    <p className='fw-light'><strong className='fw-semibold'>Age:</strong> {age}</p>
+                    <p className='fw-light'><strong className='fw-semibold'>Email:</strong> {email}</p>
+                    <p className='fw-light'><strong className='fw-semibold'>Note:</strong> {note}</p>
+                </div>
+                <div className="card-footer text-center">
+                    © 2022 - Christopher Mendes - Made with Bootstrap and React
+                </div>
             </div>
         </div>
     );
